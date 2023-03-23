8e7815601771ec4cb90a090670e8bcd1506bb3d0
Chanuth <80547770+Chanuth10@users.noreply.github.com>
2023-03-23 16:28:28 +0530

    Adding simple changes

--- src/screens/PetScreens/PetList.js
+++ src/screens/PetScreens/PetList.js
@@ -1,4 +1,4 @@
-import { StyleSheet, StatusBar } from "react-native";
+import { StyleSheet, StatusBar, Alert } from "react-native";
 
 import React, { useEffect, useState } from "react";
 import HomeHeadNav from "../../components/HomeHeadNav";
@@ -7,11 +7,10 @@ import { firebase } from "../../../Firebase/firebaseConfig";
 import { View, Text, ScrollView, TextInput, Button } from "react-native";
 import { petProfileStyles } from "./PetProfileStyles.js";
 import Card from "../../components/Card/Card";
-import { HStack, Provider } from '@react-native-material/core';
-import MaterialIcons from 'react-native-vector-icons/MaterialIcons'
+import { HStack, Provider } from "@react-native-material/core";
+import MaterialIcons from "react-native-vector-icons/MaterialIcons";
 
 const PetList = ({ navigation }) => {
-
   const [userloggeduid, setUserloggeduid] = useState(null);
   const [userdata, setUserdata] = useState(null);
 
@@ -45,31 +44,61 @@ const PetList = ({ navigation }) => {
     }
   };
 
-  console.log(userdata);
+
 
   const [petData, setPetData] = useState([]);
   const [id, setID] = useState(null);
 
   const getPets = async () => {
-    const petsRef = firebase.firestore().collection("PetData")
+    const petsRef = firebase.firestore().collection("PetData");
     petsRef.onSnapshot((snapshot) => {
       setPetData(snapshot.docs.map((doc) => doc.data()));
       setID(snapshot.docs.map((doc) => doc.id));
     });
   };
 
-  console.log(id);
+  // console.log(id);
 
   useEffect(() => {
     getPets();
     getuserdata();
   }, [userloggeduid]);
 
-  console.log(petData);
-  console.log(userloggeduid);
+  // console.log(petData);
+  // console.log(userloggeduid);
+
+  const showConfirmDialog = (id) => {
+    return Alert.alert(
+      "Are your sure?",
+      "Are you sure you want to delete the user?",
+      [
+        {
+          text: "Yes",
+          onPress: () => {
+            deleteItem(id);
+          },
+        },
+        {
+          text: "No",
+        },
+      ]
+    );
+  };
+
+  const deleteItem = (id) => {
+    firebase
+      .firestore()
+      .collection("PetData")
+      .doc(id)
+      .delete()
+      .then(() => {
+        console.log("User deleted!");
+      });
+
+      getPets();
+  };
 
   return (
-
     <View style={petProfileStyles.container}>
       <StatusBar />
       <HomeHeadNav navigation={navigation} />
@@ -82,60 +111,60 @@ const PetList = ({ navigation }) => {
           <Button
             title="Create a Pet Profile"
             color="#917DCA"
-            onPress={() => navigation.navigate('petcreation')}
-          >
-          </Button>
+            onPress={() => navigation.navigate("petcreation")}
+          ></Button>
         </View>
 
-
-        {
-          petData.map((pet, index) => {
-
-            return (
-
-              pet.userid == userloggeduid ?
-
-                <Card
-                  title={`${pet.name} the ${pet.species}`}
-                  children={
-                    <View key={index}>
-                      <HStack m={2} spacing={80} >
-                        <View>
-                          <Text style={petProfileStyles.lable}>Breed - {pet.breed}</Text>
-                          <Text style={petProfileStyles.lable}>Gender - {pet.gender}</Text>
-                          <Text style={petProfileStyles.lable}>Color - {pet.color}</Text>
-                          <Text style={petProfileStyles.lable}>DOB - {pet.dob}</Text>
-                        </View>
-                        <View style={petProfileStyles.iconContainer}>
-                          <HStack m={2} spacing={5}>
-                            <MaterialIcons
-                              name="edit"
-                              size={30}
-                            // onPress={() => navigation.navigate(CommonConstants.UPDATE_REMINDER_PATH, { reminderId: item._id })}
-                            />
-                            <MaterialIcons
-                              name="delete"
-                              size={30}
-                            // onPress={() => handleDeleteId(item._id)}
-                            />
-                          </HStack>
-                        </View>
+        {petData.map((pet, index) => {
+          return pet.userid == userloggeduid ? (
+            <Card
+              title={`${pet.name} the ${pet.species}`}
+              children={
+                <View key={index}>
+                  <HStack m={2} spacing={80}>
+                    <View>
+                      <Text style={petProfileStyles.lable}>
+                        Breed - {pet.breed}
+                      </Text>
+                      <Text style={petProfileStyles.lable}>
+                        Gender - {pet.gender}
+                      </Text>
+                      <Text style={petProfileStyles.lable}>
+                        Color - {pet.color}
+                      </Text>
+                      <Text style={petProfileStyles.lable}>
+                        DOB - {pet.dob}
+                      </Text>
+                    </View>
+                    <View style={petProfileStyles.iconContainer}>
+                      <HStack m={2} spacing={5}>
+                        <MaterialIcons
+                          name="edit"
+                          size={30}
+                          onPress={() => {
+                            navigation.navigate("petupdate", {
+                              pet: pet,
+                            });
+                          }}
+                        />
+                        <MaterialIcons
+                          name="delete"
+                          size={30}
+                          onPress={() => {
+                            showConfirmDialog(id[index]);
+                          }}
+                        />
                       </HStack>
                     </View>
-                  }
-                />
-
-                :
-
-                null
-            )
-          })
-        }
-
-
+                  </HStack>
+                </View>
+              }
+            />
+          ) : null;
+        })}
       </ScrollView>
     </View>
   );
-}
+};
 
 export default PetList;
--- src/screens/PetScreens/UpdatePet.js
+++ src/screens/PetScreens/UpdatePet.js
@@ -8,69 +8,156 @@ import { petProfileStyles } from "./PetProfileStyles.js";
 import { View, Text, ScrollView, TextInput, Button } from "react-native";
 import DropDown from "../../components/DropDown/DropDown";
 
-const UpdatePets = ({ navigation }) => {
+const UpdatePets = ({ navigation, route }) => {
+  const { pet } = route.params;
+  console.log(route);
+  const [name, setName] = useState("");
+  const [breed, setBreed] = useState("");
+  const [color, setColor] = useState("");
+  const [dob, setDob] = useState("");
+  const [spec, setSpec] = useState("");
+  const [gen, setGen] = useState("");
 
-    const [species, setSpecies] = useState([
-        { label: 'Dog', value: 'Dog' },
-        { label: 'Cat', value: 'Cat' },
-    ]);
+  const [species, setSpecies] = useState([
+    { label: "Dog", value: "Dog" },
+    { label: "Cat", value: "Cat" },
+  ]);
 
-    const [gender, setGender] = useState([
-        { label: 'Male', value: 'Male' },
-        { label: 'Female', value: 'Female' },
-    ]);
+  const [gender, setGender] = useState([
+    { label: "Male", value: "Male" },
+    { label: "Female", value: "Female" },
+  ]);
 
-    return (
-        <View style={petProfileStyles.container}>
-            <StatusBar />
-            <HomeHeadNav navigation={navigation} />
-            <View style={petProfileStyles.bottomnav}>
-                <BottomNav navigation={navigation} />
-            </View>
+  useEffect(() => {
+    setName(pet.name);
+    setBreed(pet.breed);
+    setColor(pet.color);
+    setSpec(pet.species);
+    setDob(pet.dob);
+    setGen(pet.gender);
+  }, []);
 
-            <ScrollView style={petProfileStyles.containerin}>
-                <Text style={petProfileStyles.head1}>
-                    Update Pet Profile
-                </Text>
+  const updatePet = async () => {
+    const docRef = firebase
+      .firestore()
+      .collection("PetData")
+      .where("petid", "==", pet.petid);
+    const doc = await docRef.get();
+    if (!doc.empty) {
+      if (name !== "") {
+        doc.forEach((doc) => {
+          doc.ref.update({
+            name: name,
+          });
+        });
+      }
+      if (breed !== "") {
+        doc.forEach((doc) => {
+          doc.ref.update({
+            breed: breed,
+          });
+        });
+      }
+      if (color !== "") {
+        doc.forEach((doc) => {
+          doc.ref.update({
+            color: color,
+          });
+        });
+      }
+      alert("your pet data is updated");
+    } else {
+      console.log("no pet data");
+    }
+  };
 
-                <TextInput variant="outlined" placeholder="Whity" style={petProfileStyles.inputContainer}></TextInput>
-                <Text variant='subtitle 2' style={petProfileStyles.textLableContainer}>Update Pet name</Text>
+  return (
+    <View style={petProfileStyles.container}>
+      <StatusBar />
+      <HomeHeadNav navigation={navigation} />
+      <View style={petProfileStyles.bottomnav}>
+        <BottomNav navigation={navigation} />
+      </View>
 
-                <DropDown data={species} disable={true} />
-                <Text variant='subtitle 2' style={petProfileStyles.textLableContainer}>Update Pet Species</Text>
+      <ScrollView style={petProfileStyles.containerin}>
+        <Text style={petProfileStyles.head1}>Update Pet Profile</Text>
 
-                <TextInput variant="outlined" placeholder="Labrador" style={petProfileStyles.inputContainer}></TextInput>
-                <Text variant='subtitle 2' style={petProfileStyles.textLableContainer}>Update Pet Breed</Text>
+        <TextInput
+          value={name}
+          onChangeText={setName}
+          variant="outlined"
+          placeholder="Whity"
+          style={petProfileStyles.inputContainer}
+        ></TextInput>
+        <Text variant="subtitle 2" style={petProfileStyles.textLableContainer}>
+          Update Pet name
+        </Text>
 
-                <DropDown data={gender} disable={true} />
-                <Text variant='subtitle 2' style={petProfileStyles.textLableContainer}>Update Pet Gender</Text>
+        <DropDown
+          value={spec}
+          onChange={setSpec}
+          data={species}
+          disable={true}
+        />
+        <Text variant="subtitle 2" style={petProfileStyles.textLableContainer}>
+          Update Pet Species
+        </Text>
 
-                <TextInput variant="outlined" placeholder="White" style={petProfileStyles.inputContainer}></TextInput>
-                <Text variant='subtitle 2' style={petProfileStyles.textLableContainer}>Update Pet Color</Text>
+        <TextInput
+          value={breed}
+          onChangeText={setBreed}
+          variant="outlined"
+          placeholder="Labrador"
+          style={petProfileStyles.inputContainer}
+        ></TextInput>
+        <Text variant="subtitle 2" style={petProfileStyles.textLableContainer}>
+          Update Pet Breed
+        </Text>
 
-                <TextInput variant="outlined" placeholder="20/03/2023" style={petProfileStyles.inputContainer}></TextInput>
-                <Text variant='subtitle 2' style={petProfileStyles.textLableContainerLast}>Update Pet Date of Birth</Text>
+        <DropDown data={gender} disable={true} />
+        <Text variant="subtitle 2" style={petProfileStyles.textLableContainer}>
+          Update Pet Gender
+        </Text>
 
-                <View style={petProfileStyles.cancelbuttonContainer}>
-                    <Button
-                        title="Cancel"
-                        color="#B6B3B3"
-                    // onPress={() => navigation.navigate('PetList')}
-                    >
-                    </Button>
-                </View>
+        <TextInput
+          value={color}
+          onChangeText={setColor}
+          variant="outlined"
+          placeholder="White"
+          style={petProfileStyles.inputContainer}
+        ></TextInput>
+        <Text variant="subtitle 2" style={petProfileStyles.textLableContainer}>
+          Update Pet Color
+        </Text>
 
-                <View style={petProfileStyles.editbuttonContainer}>
-                    <Button
-                        title="Submit"
-                    // onPress={handleSubmit}
-                    >
-                    </Button>
-                </View>
+        <TextInput
+          value={dob}
+          onChangeText={setDob}
+          variant="outlined"
+          placeholder="20/03/2023"
+          style={petProfileStyles.inputContainer}
+        ></TextInput>
+        <Text
+          variant="subtitle 2"
+          style={petProfileStyles.textLableContainerLast}
+        >
+          Update Pet Date of Birth
+        </Text>
 
-            </ScrollView>
+        <View style={petProfileStyles.cancelbuttonContainer}>
+          <Button
+            title="Cancel"
+            color="#B6B3B3"
+            // onPress={() => navigation.navigate('PetList')}
+          ></Button>
         </View>
-    );
+
+        <View style={petProfileStyles.editbuttonContainer}>
+          <Button title="Submit" onPress={() => updatePet()}></Button>
+        </View>
+      </ScrollView>
+    </View>
+  );
 };
 
 export default UpdatePets;
